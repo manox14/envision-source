42c213f65197ff6b2bfeba7b69ccc0932c168aec
Benjamin <benjamin.brent@gmail.com>
2011-10-03 20:13:43 +1100

    Changed description in properties

--- Envision/Properties/AssemblyInfo.cs
+++ Envision/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Envision")]
-[assembly: AssemblyDescription("Envision is a simple application for batch image processing. Envision is free and open source. If you paid for this program, you should request a refund.")]
+[assembly: AssemblyDescription("Envision is a simple application for batch image processing. Envision is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Envision")]
